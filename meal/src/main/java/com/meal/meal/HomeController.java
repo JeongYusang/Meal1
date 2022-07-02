29c1f4a9c672d131f2985685134230690c0103fa
JeongYusang <jys3875@naver.com>
2022-07-02 18:26:27 +0900

    시작시 홈페이지 메인화면 출력

--- meal/src/main/java/com/meal/meal/HomeController.java
+++ meal/src/main/java/com/meal/meal/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -23,7 +24,7 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public ModelAndView home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -32,8 +33,10 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		ModelAndView mav = new ModelAndView();
+		String viewName = "redirect:/main/main.do";
+		mav.setViewName(viewName);
+		return mav;
 	}
 	
 }
